b9eab4f6a3151ab7ff8c1075e17d1dec88978654
Igor <dutipz@ukr.net>
2022-01-01 15:00:23 +0200

    Додано 2 об'єкти

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!??");
+            Student student = new Student() { Id = 1, Name = "student1" };
+            Console.WriteLine(student.Id + " " + student.Name);
+
+            Student student2 = new Student() { Id = 2, Name = "student2" };
+            Console.WriteLine(student2.Id + " " + student2.Name);
+
+            Console.ReadLine();
         }
     }
 }
--- Student.cs
+++ Student.cs
@@ -6,5 +6,7 @@ namespace ConsoleApp1
 {
     class Student
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
