bff88466d36001a143462e67fa8101839582cc79
Igor <dutipz@ukr.net>
2022-01-01 15:12:39 +0200

    Додано клас Address

---
+++ Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Address
+    {
+        public int Id { get; set; }
+        public string StudentAddress { get; set; }
+        public int StudentId { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,6 +12,11 @@ namespace ConsoleApp1
             Student student2 = new Student() { Id = 2, Name = "student2" };
             Console.WriteLine(student2.Id + " " + student2.Name);
 
+            Address address1 = new Address() { Id = 1, StudentAddress = "student1 address", StudentId = 1 };
+
+            Console.WriteLine(address1.StudentAddress);
+
+            
             Console.ReadLine();
         }
     }
